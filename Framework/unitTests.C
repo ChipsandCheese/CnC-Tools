7ebddf2903a564092d1d8f84e6182ad204faa09f
Lillith Beatrix <lillithbeatrix@gmail.com>
2024-11-09 14:27:33 -0500

    Squashed commit of the following:
    commit 5c45105a752d4003e8facee39a3d67c03e40d6e4
    Author: Nintonito <110001155+Nintonito@users.noreply.github.com>
    Date:   Sat Nov 9 14:19:09 2024 -0500
    
        Finished getAffinity for POSIX
    
    commit d953a3440450b856922294a11db5a21bfb31db4a
    Author: Nintonito <110001155+Nintonito@users.noreply.github.com>
    Date:   Sun Oct 27 14:10:53 2024 -0400
    
        Refactored to streamline documentation.
    
    commit 4c8fe2178c855a55a4c8a097496b17b5af10fe4e
    Author: Nintonito <110001155+Nintonito@users.noreply.github.com>
    Date:   Sun Oct 27 13:08:39 2024 -0400
    
        Clean-Up
    
        Squashed some bugs and added some cleanup code for the file I/O before I expand further.
    
    commit 208d24d1b35df82e6e59f92e8597bad726a5bf8e
    Author: Nintonito <110001155+Nintonito@users.noreply.github.com>
    Date:   Sat Oct 26 13:11:07 2024 -0400
    
        Added default file name
    
        File handling has been implemented in storage header so there is no need for user input anymore.
    
    commit cfd9385261fe469bb9a41c4f93156b2205a9b556
    Author: Nintonito <110001155+Nintonito@users.noreply.github.com>
    Date:   Sat Oct 26 12:50:35 2024 -0400
    
        Added Proper File Name Handling
    
    commit d086f1d7e2eca40433a386bcadfc723695b474e8
    Merge: d74b62d 1c24bcd
    Author: beatrix1325 <lillithbeatrix@gmail.com>
    Date:   Mon Oct 21 14:40:49 2024 -0400
    
        Merge pull request #6 from ChipsandCheese/beatrix-dev
    
        Bring in Storage Unit Test

--- Framework/unitTests.C
+++ Framework/unitTests.C
@@ -2,25 +2,28 @@
  * Program Name: CnC Framework Unit Tests
  * File Name: unitTests.c
  * Date Created: October 19, 2024
- * Date Updated: October 21, 2024
- * Version: 0.2
+ * Date Updated: October 27, 2024
+ * Version: 0.2.1
  * Purpose: Unit Tests for the Framework
  */
 
+#include <stdio.h>
 #include <storage.h>
+
+#define TESTNAME "UnitTest.cnc"
+
+CnCData data = {}; //Test struct needs to be populated
 
 /*
  * Test the storage API's ability to read and write accurately
- * @Param ifile: Name for the input file being tested.
  * @Return: 0 if successful, -1 for IO error, -2 for verification failure
  */
 
-int testStorage(char *ifile) {
-    CnCData data = read_CNC(ifile);
-    if (write_CNC("writeback_test", data.resultList, data.resultCount, data.columnCount, data.columnNames) != 0)
+int testStorage()
+{
+    if (write_CNC(TESTNAME, data.resultList, data.resultCount, data.columnCount, data.columnNames) != 0)
         return -1;
-
-    CnCData data_copy = read_CNC("writeback_test");
+    CnCData data_copy = read_CNC(TESTNAME);
 
     // Compare the 2 versions of the data
     if (data.columnCount != data_copy.columnCount)
@@ -39,15 +42,13 @@ int testStorage(char *ifile) {
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Please provide a test file for the storage test\n");
-        return -1;
-    }
+int main(int argc, char *argv[])
+{
 
     int result = 0;
 
-    printf("Storage test exitted with return code %i\n", result |= testStorage(argv[1]));
+    printf("Storage test exited with return code %i\n", result |= testStorage());
+    remove(TESTNAME); //Cleans up Test Log by default.
 
     return 0;
 }
